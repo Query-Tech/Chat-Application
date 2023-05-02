e3dc7af440a48cc30a113d416dd20da582b48f08
Vishwa <srivishwa2507@gmail.com>
2023-05-02 18:22:21 +0530

    Updated

--- Backend/index.js
+++ Backend/index.js
@@ -10,13 +10,15 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose.set('strictQuery', false);
+
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("DB Connetion Successfull");
+    console.log("DB Connection Successful");
   })
   .catch((err) => {
     console.log(err.message);
@@ -28,6 +30,7 @@ app.use("/api/messages", messageRoutes);
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
+
 const io = socket(server, {
   cors: {
     origin: "http://localhost:3000",
